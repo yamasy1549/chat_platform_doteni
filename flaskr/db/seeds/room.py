e801ae45049a43dbe3493d8c70efa267a966e512
yamasy1549 <sanae@yamasy.info>
2021-08-24 20:55:37 +0900

    seedデータを追加

--- flaskr/db/seeds/room.py
+++ flaskr/db/seeds/room.py
@@ -1,10 +1,30 @@
-rooms = [
-        ("ルーム1", 2),
-        ("ルーム2", 2),
-        ("ルーム3", 2),
-        ("ルーム4", 2),
-        ("ルーム5", 3),
-        ("ルーム6", 3),
-        ("ルーム7", 4),
-        ("ルーム8", 4),
-        ]
+import os
+
+
+def load_csv_data(csv_file):
+    import csv
+
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        csv_data = [row for row in reader]
+
+    rooms = [(name, int(room_id)) for name, room_id in csv_data]
+
+    return rooms
+
+
+csv_file = "flaskr/db/seeds/csv/rooms.csv"
+
+if os.path.exists(csv_file):
+    rooms = load_csv_data(csv_file)
+else:
+    rooms = [
+            ("ルーム1", 2),
+            ("ルーム2", 2),
+            ("ルーム3", 2),
+            ("ルーム4", 2),
+            ("ルーム5", 3),
+            ("ルーム6", 3),
+            ("ルーム7", 4),
+            ("ルーム8", 4),
+            ]
--- flaskr/db/seeds/room_scenario.py
+++ flaskr/db/seeds/room_scenario.py
@@ -1,10 +1,30 @@
-room_scenarios = [
-        (1, 1),
-        (1, 2),
-        (2, 1),
-        (2, 2),
-        (3, 3),
-        (3, 4),
-        (4, 3),
-        (4, 4),
-        ]
+import os
+
+
+def load_csv_data(csv_file):
+    import csv
+
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        csv_data = [row for row in reader]
+
+    room_scenarios = [(int(room_id), int(scenario_id)) for room_id, scenario_id in csv_data]
+
+    return room_scenarios
+
+
+csv_file = "flaskr/db/seeds/csv/room_scenarios.csv"
+
+if os.path.exists(csv_file):
+    room_scenarios = load_csv_data(csv_file)
+else:
+    room_scenarios = [
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (2, 2),
+            (3, 3),
+            (3, 4),
+            (4, 3),
+            (4, 4),
+            ]
