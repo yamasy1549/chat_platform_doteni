cc7f0ce56b5841162d2f8f68a5de1c61b358da24
yamasy1549 <sanae@yamasy.info>
2021-08-31 18:55:35 +0900

    デプロイ方法まとめ

--- development.sh
+++ development.sh
@@ -1,4 +1,19 @@
 docker context use default
 
+# Dockerコンテナ (uwsgi, nginx) を終了するには
+docker-compose -f docker-compose.yml -f docker-compose.development.yml down
+
+# Dockerコンテナ (uwsgi, nginx) を起動するには
 docker-compose -f docker-compose.yml -f docker-compose.development.yml up
-# docker-compose -f docker-compose.yml -f docker-compose.production.yml exec uwsgi flask init_db_with_seeds
+
+# Dockerコンテナ (uwsgi, nginx) を起動し、そのプロセスをデーモン化するには
+# docker-compose -f docker-compose.yml -f docker-compose.development.yml up -d
+
+# 起動中のDockerコンテナ (uwsgi) の中に入るには
+# docker-compose -f docker-compose.yml -f docker-compose.development.yml exec --rm -it uwsgi /bin/bash
+
+# 起動中のDockerコンテナ (uwsgi) でコマンドを実行 (flask init_db_with_seeds) するには
+# docker-compose -f docker-compose.yml -f docker-compose.development.yml exec --rm -it uwsgi flask init_db_with_seeds
+
+# Dockerコンテナ (uwsgi, nginx) を停止するには
+# docker-compose -f docker-compose.yml -f docker-compose.development.yml down
--- production.sh
+++ production.sh
@@ -1,29 +1,29 @@
-#
-# Docker image のビルド
-#
+# DockerイメージをビルドしてECRへでアップロード
 docker context use default
-
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com
 
 docker build -t chat_platform_doteni_uwsgi:latest -f ./config_uwsgi/Dockerfile.production .
-docker tag chat_platform_doteni_uwsgi:latest 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com/chat_platform_doteni_uwsgi:latest
-docker push 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com/chat_platform_doteni_uwsgi:latest
-
 docker build -t chat_platform_doteni_nginx:latest -f ./config_nginx/Dockerfile.production .
+docker tag chat_platform_doteni_uwsgi:latest 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com/chat_platform_doteni_uwsgi:latest
 docker tag chat_platform_doteni_nginx:latest 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com/chat_platform_doteni_nginx:latest
-docker push 154234079813.dkr.ecr.ap-northeast-1.amazonaws.com/chat_platform_doteni_nginx:latest
+docker-compose -f docker-compose.yml -f docker-compose.production.yml push
 
-# docker-compose push
 
-#
-# ECSへのデプロイ
-#
+# （初回のみ）ECSでクラスタを作成するには
+# ecs-cli up --capability-iam --instance-type t2.medium --keypair キーペア名
+
+
+# ECSへデプロイ
 docker context use myecscontext
-
-# ecs-cli up --capability-iam --instance-type c1.xlarge --force --keypair chat_platform_doteni
+ecs-cli compose -f docker-compose.yml -f docker-compose.production.yml service down
+sleep 60
 ecs-cli compose -f docker-compose.yml -f docker-compose.production.yml service up
 ecs-cli compose -f docker-compose.yml -f docker-compose.production.yml service ps
-
 docker context use default
 
-# ssh -i ~/Downloads/chat_platform_doteni.pem ec2-user@18.183.79.200
+# 起動中のサーバの中に入るには
+# ssh -i chat_platform_doteni.pem ec2-user@XX.XX.XX.XX
+
+# 起動中のサーバの中で、起動中のDockerコンテナの中に入るには
+# docker ps   # コンテナのIDを確認
+# docker exec --it コンテナのID /bin/bash
